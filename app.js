9e86fd35eae8b84203fbe1d03618fd26cf3c990c
Lukinaa <lukinaa82@gmail.com>
2022-02-20 19:54:31 +0300

    thml to ejs + css

--- app.js
+++ app.js
@@ -3,23 +3,32 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+
+app.set('view engine', 'ejs');
+
+
 // listen for
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root:__dirname});
+    const blogs = [
+        {title: "Yoshi makarosi", snippet: "I'm makaroshi"},
+        {title: "Mario udario", snippet: "I'm udario"},
+        {title: "How make app", snippet: "I'm make app"},
+    ];
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root:__dirname});
+    res.render('about', { title: 'About' });
 });
 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
-
+app.get('/blogs/create', (req,res)=>{
+    res.render('create', { title: 'Create a new blog' });
+})
 
 // last method
 app.use((req, res)=>{
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404', { title: '404' });
 });
